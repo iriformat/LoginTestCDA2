1237473e9a20463aea0b35a3c19cec260f7ed0cc
iriformat <iriformat@gmail.com>
2023-03-17 08:32:09 +0100

    new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home';
+import { AuthGuard } from './_helpers';
+
+const accountModule = () =>
+  import('./account/account.module').then((x) => x.AccountModule);
+const usersModule = () =>
+  import('./users/users.module').then((x) => x.UsersModule);
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
+  { path: 'account', loadChildren: accountModule },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
